bdcb5ae444efb537953e00fc912c330433d6a118
Bell0-0 <vasquezdayanara67@gmail.com>
2025-06-09 20:16:45 -0600

    Update love.js

--- js/love.js
+++ js/love.js
@@ -56,7 +56,7 @@ let blocks = document.getElementsByClassName("block"),
     index = 0, 
     clone_block;    
 
-block.style.top = "30%";
+block.style.top = "50%";
 block.style.left = "50%";
 block.style.margin = "-20px 0 0 -20px";
 
